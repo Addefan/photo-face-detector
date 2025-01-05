1f64f6abd54b63de8b5a8ad32854188c3f77f6ab
Vitaly Zorin <addefan@mail.ru>
2025-01-05 04:21:23 +0300

    feat(terraform): add api gateway for faces hosting

--- terraform/bot.tf
+++ terraform/bot.tf
@@ -1,5 +1,5 @@
 resource "yandex_function" "bot" {
-  name               = "vvot00-boot"
+  name               = "vvot26-boot"
   entrypoint         = "index.handler"
   memory             = "128"
   runtime            = "python312"
@@ -9,6 +9,7 @@ resource "yandex_function" "bot" {
     TELEGRAM_BOT_TOKEN = var.tg_bot_key
     PHOTOS_MOUNT_POINT = yandex_storage_bucket.photos_bucket.bucket
     FACES_MOUNT_POINT  = yandex_storage_bucket.faces_bucket.bucket
+    API_GATEWAY_URL    = "https://${yandex_api_gateway.api_gw.domain}"
   }
   content {
     zip_filename = data.archive_file.bot_source.output_path
@@ -39,6 +40,31 @@ resource "yandex_function_iam_binding" "exam_solver_tg_bot_iam" {
 
 resource "telegram_bot_webhook" "exam_solver_tg_bot_webhook" {
   url = "https://functions.yandexcloud.net/${yandex_function.bot.id}"
+}
+
+resource "yandex_api_gateway" "api_gw" {
+  name = "vvot26-apigw"
+  spec = <<-EOT
+openapi: "3.0.0"
+info:
+  version: 1.0.0
+  title: Photo Face Detector API
+paths:
+  /:
+    get:
+      summary: Serve face images from Yandex Cloud Object Storage
+      parameters:
+        - name: face
+          in: query
+          required: true
+          schema:
+            type: string
+      x-yc-apigateway-integration:
+        type: object_storage
+        bucket: ${yandex_storage_bucket.faces_bucket.bucket}
+        object: "{face}"
+        service_account_id: ${yandex_iam_service_account.sa_bot.id}
+EOT
 }
 
 data "archive_file" "bot_source" {
