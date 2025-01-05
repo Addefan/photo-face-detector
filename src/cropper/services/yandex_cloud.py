eda2d212b026f549fbb8bef2b5291fba61ec7d3a
Vitaly Zorin <addefan@mail.ru>
2025-01-05 04:20:12 +0300

    feat(cropper): add changing mimetype to jpeg after save face to bucket via filesystem

--- src/cropper/services/yandex_cloud.py
+++ src/cropper/services/yandex_cloud.py
@@ -1,5 +1,11 @@
+from time import sleep
+
 import piexif
+import requests
 from PIL import Image
+from requests_aws4auth import AWS4Auth
+
+from settings import OBJECT_STORAGE_API_URL, FACES_MOUNT_POINT, ACCESS_KEY_ID, SECRET_ACCESS_KEY
 
 
 def get_image(image_path):
@@ -8,5 +14,26 @@ def get_image(image_path):
     return image
 
 
+def set_mime_type(bucket, object_key, mime_type):
+    url = f"{OBJECT_STORAGE_API_URL}/{bucket}/{object_key}"
+
+    headers = {
+        "Content-Type": mime_type,
+        "X-Amz-Copy-Source": f"/{bucket}/{object_key}",
+        "X-Amz-Metadata-Directive": "REPLACE",
+    }
+
+    auth = AWS4Auth(
+        ACCESS_KEY_ID,
+        SECRET_ACCESS_KEY,
+        "ru-central1",
+        "s3",
+    )
+
+    requests.put(url, headers=headers, auth=auth)
+
+
 def save_image(image, image_path, exif=()):
     image.save(image_path, exif=piexif.dump(exif))
+    sleep(1)
+    set_mime_type(FACES_MOUNT_POINT, image_path.name, "image/jpeg")
